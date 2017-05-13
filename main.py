f98e1a0aad202ccebad209f918880c15874e342a
Ubuntu <ubuntu@ip-172-31-35-227.ec2.internal>
2017-05-13 19:45:46 +0000

    Got a connection to the Monogodb database

--- main.py
+++ main.py
@@ -4,15 +4,13 @@ from datetime import datetime
 
 database = "cluster0-shard-00-02-zolhc.mongodb.net:27017"
 password = "fuckFacebook69"
-uri = "mongodb://D2_Dev:{}@cluster0-shard-00-00-zolhc.mongodb.net:27017, " + \
-	"cluster0-shard-00-01-zolhc.mongodb.net:27017,cluster0-shard-00-02-zolhc.mongodb.net:27017/" + \
-	"{}?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin".format(password, database)
+uri = "mongodb://BigMig:IsthisagoodPassword?1@ds159517.mlab.com:59517/down2"
 try:
-	conn = pymongo.MongoClient(uri)
+	client = pymongo.MongoClient(uri)
 	# force connection on a request as the
 	# connect=True parameter of MongoClient seems
 	# to be useless here
-	print conn.server_info()
+	#print client.server_info()
 except pymongo.errors.ServerSelectionTimeoutError as err:
 	print err
 except:
@@ -20,29 +18,34 @@ except:
 	print pymongo.errors.PyMongoError.message
 	print "FAILED"
 
-def main():
-	#createTable()
-	db = conn.myFirstMB
-	add_country(db)
-	print get_country(db)
-
-def createTable():
-	# conn.runCommand({
-	# 	"create": "test"
-	# })
-	db = conn.test;
-	print db.test.insert_one({
-		"user": "mig"
-	})
-
-	print db.find()
-
-def add_country(db):
-    db.countries.insert({"name" : "Canada"})
-
-def get_country(db):
-    return db.countries.find_one()
-
+db = client.down2
+posts = db.posts
+post_data = {
+    'title': 'Python and MongoDB',
+    'content': 'PyMongo is fun, you guys',
+    'author': 'Scott'
+}
+result = posts.insert_one(post_data)
+print('One post: {0}'.format(result.inserted_id))
+post_1 = {
+    'title': 'Python and MongoDB',
+    'content': 'PyMongo is fun, you guys',
+    'author': 'Scott'
+}
+post_2 = {
+    'title': 'Virtual Environments',
+    'content': 'Use virtual environments, you guys',
+    'author': 'Scott'
+}
+post_3 = {
+    'title': 'Learning Python',
+    'content': 'Learn Python, it is easy',
+    'author': 'Bill'
+}
+new_result = posts.insert_many([post_1, post_2, post_3])
+print('Multiple posts: {0}'.format(new_result.inserted_ids))
+bills_post = posts.find_one({'author': 'Bill'})
+print bills_post['content']
 
 if __name__ == "__main__":
 	# main()
